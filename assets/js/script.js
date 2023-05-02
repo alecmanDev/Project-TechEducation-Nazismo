29ad817c181756682441f476e86eed8ff81db976
Alessandro Alves <alvesalessandro.school@gmail.com>
2023-05-01 22:22:09 -0300

    30% do Projeto Concluído;
    Aqui nesse projeto comecei com a página principal!

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.nav-bar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.toggle('active')
+}
+
+window.onscroll = () =>{
+  navbar.classList.remove('active');
+}
