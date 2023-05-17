8763097d4ea0aa55308f96f880dc2ab4eeb57877
Alessandro Alves <alvesalessandro.school@gmail.com>
2023-05-17 13:09:30 -0300

    Forms ADD
    Formulário concluído, só falta fazer o mecanismo de banco de dados e LGPD

---
+++ assets/js/inscription.js
@@ -0,0 +1,76 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const number = document.getElementById("number");
+const classroom = document.getElementById("classroom");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const numberValue = number.value;
+  const classroomValue = classroom.value;
+
+  if (usernameValue === "") {
+    setErrorFor(username, "O nome de usuário é obrigatório.");
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "O email é obrigatório.");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido.");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (numberValue === "") {
+    setErrorFor(number, "O número é obrigatório.");
+  } else if (numberValue.length < 11) {
+    setErrorFor(number, "Exemplo: 91932205050.");
+  } else {
+    setSuccessFor(number);
+  }
+
+  if (classroom === "") {
+    setErrorFor(classroom, "Indique sua turma");
+  } else {
+    setSuccessFor(classroom);
+  }
+
+  const formControls = form.querySelectorAll(".form-control");
+
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success";
+  });
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  // Adiciona a mensagem de erro
+  small.innerText = message;
+
+  // Adiciona a classe de erro
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  // Adicionar a classe de sucesso
+  formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
